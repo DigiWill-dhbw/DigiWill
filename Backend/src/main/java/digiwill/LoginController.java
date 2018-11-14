a31a485a50538da6453651884961ebbec80523bd
Jannik Moell <jannik.moell@sap.com>
2018-11-14 11:10:35 +0100

    [Backend][Feat] DW-39 First try

--- Backend/src/main/java/digiwill/LoginController.java
+++ Backend/src/main/java/digiwill/LoginController.java
@@ -4,13 +4,13 @@ import org.springframework.web.bind.annotation.*;
 
 public class LoginController {
 
-    @RequestMapping("/login")
+   /* @RequestMapping("/login")
     @ResponseBody
     public String login(@ModelAttribute("loginData") LoginData loginData ) { //TODO implement
         //System.out.println(loginData.getUsername());
         //System.out.println(loginData.getPasswordHash());
         return "Hello";
-    }
+    }*/
 
     @RequestMapping("/reset_pw")
     @ResponseBody
