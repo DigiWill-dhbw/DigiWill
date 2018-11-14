fbb68b9643a519c57192bd619c622ded3a36131b
Morten Harter <mortenharter@gmail.com>
2018-11-14 12:42:11 +0100

    [Backend][Feat] DW-40
    Mime Type now depend on html flag

--- Backend/src/main/java/digiwill/EmailDispatcher.java
+++ Backend/src/main/java/digiwill/EmailDispatcher.java
@@ -1,5 +1,7 @@
 package digiwill;
 
+import org.springframework.http.MediaType;
+
 import javax.mail.*;
 import javax.mail.internet.*;
 import java.util.Date;
@@ -14,38 +16,6 @@ public class EmailDispatcher {
         } catch (EmailException e) {
             throw new EmailException("Failed to send registration Email", e);
         }
-    }
-
-    public void sendEmail(List<String> recipients, String subject, boolean htmlContentFlag, String content) throws EmailException {//TODO implement
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", System.getProperty("mail.smtp.host"));
-        props.put("mail.smtp.port", System.getProperty("mail.smtp.port"));
-
-        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(System.getProperty("mail.adress"), System.getProperty("mail.password"));
-            }
-        });
-        Message msg = new MimeMessage(session);
-        try {
-            msg.setFrom(new InternetAddress(System.getProperty("mail.adress"),false));
-            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(String.join(",", recipients), true));
-            msg.setSubject(subject);
-            msg.setContent(content, "text/html");
-            msg.setSentDate(new Date());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        try {
-            Transport.send(msg);
-        } catch (MessagingException e) {
-            System.err.println("Error sending Email.");
-            e.printStackTrace();
-        }
-
     }
 
     public void sendResetEmail(EmailResponseHandle responseHandle) throws EmailException {//TODO implement
@@ -66,5 +36,36 @@ public class EmailDispatcher {
         }
     }
 
+    public void sendEmail(List<String> recipients, String subject, boolean htmlContentFlag, String content) throws EmailException {//TODO implement
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", System.getProperty("mail.smtp.host"));
+        props.put("mail.smtp.port", System.getProperty("mail.smtp.port"));
+
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(System.getProperty("mail.adress"), System.getProperty("mail.password"));
+            }
+        });
+        Message msg = new MimeMessage(session);
+        try {
+            msg.setFrom(new InternetAddress(System.getProperty("mail.adress"),false));
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(String.join(",", recipients), true));
+            msg.setSubject(subject);
+            msg.setContent(content, htmlContentFlag ? MediaType.TEXT_HTML_VALUE:MediaType.TEXT_PLAIN_VALUE);
+            msg.setSentDate(new Date());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Transport.send(msg);
+        } catch (MessagingException e) {
+            System.err.println("Error sending Email.");
+            e.printStackTrace();
+        }
+
+    }
 
 }
