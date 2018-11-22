9e1e615a5f49ae9c21c221723c7c4afeaae0649e
Morten Harter <mortenharter@gmail.com>
2018-11-22 10:55:24 +0100

    [Backend][Feat]DW-59 Properties file can be loaded
    File for passwords and urls etc. is now called secrets-{x}.properties whereas x is variable and set in VM options with "-DenvTarget=x"

---
+++ Backend/src/main/java/digiwill/Config.java
@@ -0,0 +1,31 @@
+package digiwill;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@PropertySource("classpath:secrets-${envTarget}.properties")
+public class Config {
+    @Autowired
+    Environment env;
+
+    Logger logger = LoggerFactory.getLogger(Config.class);
+    @Bean("emailconfig")
+    public EmailConfig getEmailConfig(){
+        logger.error(env.getProperty("mail.host")+
+                env.getProperty("mail.port")+
+                env.getProperty("mail.user")+
+                env.getProperty("mail.password"));
+       return new EmailConfig(env.getProperty("mail.host"),
+               env.getProperty("mail.port"),
+               env.getProperty("mail.user"),
+               env.getProperty("mail.password"));
+    }
+
+}
--- Backend/src/main/java/digiwill/EmailDispatcher.java
+++ Backend/src/main/java/digiwill/EmailDispatcher.java
@@ -1,6 +1,9 @@
 package digiwill;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
 
 
 import javax.mail.Message;
@@ -10,28 +13,33 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import javax.validation.constraints.Email;
 import java.util.Date;
 import java.util.List;
 import java.util.Properties;
 
+@Service
 public class EmailDispatcher {
-    Session session;
 
-    EmailDispatcher() {
+    private Logger logger = LoggerFactory.getLogger(EmailDispatcher.class);
+    private final EmailConfig emailconfig;
+    private Session session;
+    public EmailDispatcher(EmailConfig emailconfig) {
+        this.emailconfig = emailconfig;
+        logger.debug(emailconfig.getHost()+emailconfig.getPort()+emailconfig.getUser()+emailconfig.getPassword());
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", System.getProperty("mail.smtp.host"));
-        props.put("mail.smtp.port", System.getProperty("mail.smtp.port"));
+        props.put("mail.smtp.host", emailconfig.getHost());
+        props.put("mail.smtp.port", emailconfig.getPort());
 
-        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+        session = Session.getInstance(props, new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(System.getProperty("mail.adress"), System.getProperty("mail.password"));
+                return new PasswordAuthentication(emailconfig.getUser(), emailconfig.getPassword());
             }
         });
     }
     public void sendRegistrationConfirmationEmail(EmailResponseHandle responseHandle) throws EmailException {
+        logger.debug("Sending registration email.");
         String subject = "Confirm your registration!";
         String content = "Hello "+responseHandle.getUserHandle().getAlias()+"<br/><br/>"+
                 "please confirm your registration by clicking <a href=\"https://registrierung.com\">this link</a>.<br/>"+
@@ -56,6 +64,7 @@ public class EmailDispatcher {
     }
 
     public void sendReminderEmail(UserHandle userHandle) throws EmailException {
+        logger.debug("Sending Reminder");
         String subject = "Are you dead?";
         String content = "Hello "+userHandle.getAlias()+",<br/>"+
                 "we have noticed you haven't checked in with us for a long time.<br/>"+
@@ -74,24 +83,27 @@ public class EmailDispatcher {
         sendEmail(String.join(",", recipients), subject, htmlContentFlag, content);
     }
     public void sendEmail(String recipient, String subject, boolean htmlContentFlag, String content) throws EmailException {
+        logger.debug("Sending Email");
         Message msg = new MimeMessage(this.session);
         try {
-            msg.setFrom(new InternetAddress(System.getProperty("mail.adress"),false));
+            msg.setFrom(new InternetAddress(emailconfig.getUser(),false));
             msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient, true));
             msg.setSubject(subject);
             msg.setContent(content, htmlContentFlag ? MediaType.TEXT_HTML_VALUE:MediaType.TEXT_PLAIN_VALUE);
             msg.setSentDate(new Date());
         } catch (Exception e) {
+            logger.error("Error creating mail.");
             e.printStackTrace();
         }
 
         try {
             Transport.send(msg);
         } catch (MessagingException e) {
-            System.err.println("Error sending Email.");
+            logger.error("Error sending mail.");
             e.printStackTrace();
         }
 
     }
 
 }
+
