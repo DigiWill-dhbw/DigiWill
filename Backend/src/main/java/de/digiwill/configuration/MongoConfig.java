cd9698245a358a22ea87977cbeab61d95c80fb6f
Morten Harter <mortenharter@gmail.com>
2018-11-23 15:29:44 +0100

    [Backend][Misc]DW-62 mongodb connection data now in properites file

--- Backend/src/main/java/de/digiwill/configuration/MongoConfig.java
+++ Backend/src/main/java/de/digiwill/configuration/MongoConfig.java
@@ -9,10 +9,6 @@ import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 @Configuration
 public class MongoConfig {
 
-    public @Bean
-    MongoDbFactory mongoDbFactory() {
-        //TODO move constants to property files
-        return new SimpleMongoDbFactory(new MongoClient("localhost", 27017), "digiwill");
-    }
+
 
 }
