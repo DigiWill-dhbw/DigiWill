c144884d0783e6a7b74569bc3a2bf244015d56ca
Morten Harter <mortenharter@gmail.com>
2018-11-30 18:11:23 +0100

    [Backend][Misc]DW-71 added more email test, email addresses now get verfied with regex before email is sent
    if email is not valid throws EmailException

--- Backend/src/main/java/de/digiwill/util/EmailDispatcher.java
+++ Backend/src/main/java/de/digiwill/util/EmailDispatcher.java
@@ -15,7 +15,6 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Date;
 import java.util.List;
-
 public class EmailDispatcher {
 
     private Logger logger = LoggerFactory.getLogger(EmailDispatcher.class);
@@ -29,15 +28,12 @@ public class EmailDispatcher {
     }
 
     public void sendRegistrationConfirmationEmail(EmailResponseHandle responseHandle) throws EmailException {
-        logger.debug("Sending registration email.");
-        String subject = "Confirm your registration!";
-        String content = "Hello "+responseHandle.getUserHandle().getAlias()+"<br/><br/>"+
-                "please confirm your registration by clicking <a href=\"https://registrierung.com\">this link</a>.<br/>"+
-                "Thanks for using our service<br/><br/>"+
-                "Regards, <br/>DigiWill";
+        logger.debug("Initiating sendRegistrarionConfirmation");
+        String subject = REGISTRATION_EMAIL_SUBJECT;
+        String content = REGISTRATION_EMAIL_CONTENT.replaceAll("<username>", responseHandle.getUserHandle().getUsername());
 
         try {
-            sendEmail(responseHandle.getUserHandle().getEmailAddress() , subject, true, content);
+            sendEmail(responseHandle.getUserHandle().getUsername() , subject, true, content);
         } catch (EmailException e) {
             throw new EmailException("Failed to send registration Email", e);
         }
@@ -54,16 +50,13 @@ public class EmailDispatcher {
     }
 
     public void sendReminderEmail(UserHandle userHandle) throws EmailException {
-        logger.debug("Sending Reminder");
-        String subject = "Are you dead?";
-        String content = "Hello "+userHandle.getAlias()+",<br/>"+
-                "we have noticed you haven't checked in with us for a long time.<br/>"+
-                "Please confirm that you are alive in your app or at <a href=\"https://google.de\">this website</a>.<br/><br/>"+
-                "Regards, <br/>DigiWill";
+        logger.debug("Initiating sendReminder");
+        String subject = REMINDER_EMAIL_SUBJECT;
+        String content = REMINDER_EMAIL_CONTENT.replaceAll("<username>", userHandle.getUsername());
         //TODO generate Link for user and refactor message content into file
 
         try {
-            sendEmail(userHandle.getEmailAddress() , subject, true, content);
+            sendEmail(userHandle.getUsername() , subject, true, content);
         } catch (EmailException e) {
             throw new EmailException("Failed to send reminder", e);
         }
@@ -74,29 +67,49 @@ public class EmailDispatcher {
     }
     public void sendEmail(String recipient, String subject, boolean htmlContentFlag, String content) throws EmailException {
         logger.debug("Creating Email");
-        Message message = new MimeMessage(session);
-        try {
-            message.setFrom(new InternetAddress(session.getProperty("mail.smtp.host"),false));
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient, true));
-            message.setSubject(subject);
-            message.setContent(content, htmlContentFlag ? MediaType.TEXT_HTML_VALUE:MediaType.TEXT_PLAIN_VALUE);
-            message.setSentDate(new Date());
-        } catch (Exception e) {
-            logger.error("Error creating mail.");
-            e.printStackTrace();
-            throw new EmailException(e.getMessage());
-        }
-        logger.debug("Sending Email");
-        try {
-            emailTransportWrapper.sendMessage(message);
-        } catch (MessagingException e) {
-            logger.error("Error sending mail.");
-            e.printStackTrace();
-            throw new EmailException(e.getMessage());
-        }
+        if(recipient.matches(EMAIL_REGEX)) {
+            Message message = new MimeMessage(session);
+            try {
+                message.setFrom(new InternetAddress(session.getProperty("mail.smtp.host"), false));
+                message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient, true));
+                message.setSubject(subject);
+                message.setContent(content, htmlContentFlag ? MediaType.TEXT_HTML_VALUE : MediaType.TEXT_PLAIN_VALUE);
+                message.setSentDate(new Date());
+            } catch (Exception e) {
+                logger.error("Error creating mail.");
+                e.printStackTrace();
+                throw new EmailException(e.getMessage());
+            }
+            logger.debug("Sending Email");
+            try {
+                emailTransportWrapper.sendMessage(message);
+            } catch (MessagingException e) {
+                logger.error("Error sending mail.");
+                e.printStackTrace();
+                throw new EmailException(e.getMessage());
+            }
 
-        logger.debug("Email sent");
+            logger.debug("Email sent");
+        }else{
+            logger.error("Bad email recipient");
+            throw new EmailException("Bad email recipient");
+        }
     }
+
+
+    public static final String RESET_EMAIL_SUBJECT= "";
+    public static final String RESET_EMAIL_CONTENT = "";
+    public static final String REGISTRATION_EMAIL_SUBJECT = "Confirm your registration!";
+    public static final String REGISTRATION_EMAIL_CONTENT = "Hello <username><br/><br/>"+
+            "please confirm your registration by clicking <a href=\"https://registrierung.com\">this link</a>.<br/>"+
+            "Thanks for using our service<br/><br/>"+
+            "Regards, <br/>DigiWill";;
+    public static final String REMINDER_EMAIL_SUBJECT = "Are you dead?";
+    public static final String REMINDER_EMAIL_CONTENT = "Hello <username>,<br/>"+
+            "we have noticed you haven't checked in with us for a long time.<br/>"+
+            "Please confirm that you are alive in your app or at <a href=\"https://google.de\">this website</a>.<br/><br/>"+
+            "Regards, <br/>DigiWill";
+    public static final String EMAIL_REGEX = "^([A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,},?)+$";
 
 }
 
