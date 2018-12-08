6431e5bc21e58ea5f5c5ce03b743e441d7c955c3
Kucki99 <kuck.robin@gmail.com>
2018-12-08 14:09:29 +0100

    [Backend][Misc] DW-91 added selenium driver for linux os

--- Backend/src/test/java/de/digiwill/util/SeleniumDriverUtils.java
+++ Backend/src/test/java/de/digiwill/util/SeleniumDriverUtils.java
@@ -5,15 +5,17 @@ import org.apache.commons.lang3.SystemUtils;
 public class SeleniumDriverUtils {
 
     private static String CHROME_DRIVER_WINDOWS = "..\\Backend\\\\driver\\\\chromedriver.exe";
-    private static String CHROME_DRIVER_MAC_OSX = "../Backend/driver/chromedriver";
+    private static String CHROME_DRIVER_MAC_OSX = "../Backend/driver/chromedriver_osx";
+    private static String CHROME_DRIVER_LINUX = "../Backend/driver/chromedriver_linux";
 
     public static String getChromeDriverPath() {
         if (SystemUtils.IS_OS_WINDOWS) {
             return CHROME_DRIVER_WINDOWS;
-        } else if(SystemUtils.IS_OS_MAC_OSX){
+        } else if (SystemUtils.IS_OS_MAC_OSX) {
             return CHROME_DRIVER_MAC_OSX;
+        } else if (SystemUtils.IS_OS_LINUX) {
+            return CHROME_DRIVER_LINUX;
         } else {
-            //TODO add linux driver
             return "";
         }
     }
