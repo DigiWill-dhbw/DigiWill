81694e2858d3425941ec8c16d34636caaf5a66a0
Kucki99 <kuck.robin@gmail.com>
2018-12-11 10:51:21 +0100

    [Backend][Feat] DW-72 added controller for send life sign

---
+++ Backend/src/main/java/de/digiwill/controller/SendLifeSignController.java
@@ -0,0 +1,28 @@
+package de.digiwill.controller;
+
+import de.digiwill.model.UserHandle;
+import de.digiwill.repository.UserHandleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.security.Principal;
+import java.time.Instant;
+import java.util.Date;
+
+@Controller
+public class SendLifeSignController {
+
+    @Autowired
+    UserHandleRepository repository;
+
+    @PostMapping("/lifesign")
+    void sendLifeSign(Principal principal) {
+        String username = principal.getName();
+        UserHandle userHandle = repository.findUserHandleByUsername(username);
+        long currentTime = new Date().getTime();
+        userHandle.setLastSignOfLife(currentTime);
+        repository.save(userHandle);
+    }
+
+}
