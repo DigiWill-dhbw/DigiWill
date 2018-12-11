afd1984bc327e5b1533332c6ee64e06bdcea6c18
Kucki99 <kuck.robin@gmail.com>
2018-12-11 11:15:19 +0100

    [Backend][Misc] DW-72 user handle repository -> user handle manager

--- Backend/src/main/java/de/digiwill/controller/EmailController.java
+++ Backend/src/main/java/de/digiwill/controller/EmailController.java
@@ -3,7 +3,7 @@ package de.digiwill.controller;
 import de.digiwill.model.BaseAction;
 import de.digiwill.model.EmailAction;
 import de.digiwill.model.UserHandle;
-import de.digiwill.repository.UserHandleRepository;
+import de.digiwill.model.UserHandleManager;
 import de.digiwill.service.RegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,19 +19,20 @@ import java.util.List;
 
 @Controller
 public class EmailController {
+
     @Autowired
-    UserHandleRepository repository;
-    @Autowired
+    UserHandleManager userHandleManager;
     RegistrationService registrationService;
 
     @GetMapping("/addEmail")
     public String addEmail(Model model) {
         return "add_email";
     }
+
     @PostMapping("/addEmail")
-    public RedirectView addEmailPost(@RequestParam(name="recipients", required=true) String recipients, @RequestParam(name="subject", required=true) String subject, @RequestParam(name="content", required=true) String content, Principal principal, Model model){
+    public RedirectView addEmailPost(@RequestParam(name = "recipients", required = true) String recipients, @RequestParam(name = "subject", required = true) String subject, @RequestParam(name = "content", required = true) String content, Principal principal, Model model) {
         String username = principal.getName();
-        UserHandle user = repository.findUserHandleByUsername(username);
+        UserHandle user = userHandleManager.loadUserByUserName(username);
         List<String> recipient_list = new ArrayList<>();
         String[] reci_array = recipients.split(" ");
         for (String r :
@@ -45,35 +46,38 @@ public class EmailController {
         }
         EmailAction action = new EmailAction(recipient_list, subject, false, content);
         user.addAction(action);
-        repository.save(user);
+        userHandleManager.updateUser(user);
         return new RedirectView("getEmails");
     }
+
     @GetMapping("/getEmails")
     public String getEmails(Model model, Principal principal) {
-        UserHandle user = repository.findUserHandleByUsername(principal.getName());
-        List<BaseAction> actions= user.getActions();
+        UserHandle user = userHandleManager.loadUserByUserName(principal.getName());
+        List<BaseAction> actions = user.getActions();
         List<EmailAction> emails = new ArrayList<>();
-        for (BaseAction action: actions
+        for (BaseAction action : actions
         ) {
             emails.add((EmailAction) action);
         }
         model.addAttribute("emails", emails);
         return "get_emails";
     }
+
     @GetMapping("/editEmail")
-    public String editEmail(@RequestParam(name="idx", required=true) String index, Principal principal, Model model) {
+    public String editEmail(@RequestParam(name = "idx", required = true) String index, Principal principal, Model model) {
         int idx = Integer.parseInt(index);
-        UserHandle user = repository.findUserHandleByUsername(principal.getName());
-        List<BaseAction> actions= user.getActions();
+        UserHandle user = userHandleManager.loadUserByUserName(principal.getName());
+        List<BaseAction> actions = user.getActions();
         EmailAction action = (EmailAction) actions.get(idx);
         model.addAttribute("email", action);
         return "edit_email";
     }
+
     @PostMapping("/editEmail")
-    public RedirectView editEmailPost(@RequestParam(name="recipients", required=true) String recipients, @RequestParam(name="subject", required=true) String subject, @RequestParam(name="content", required=true) String content, @RequestParam(name="index", required=true) String index,Principal principal, Model model) {
+    public RedirectView editEmailPost(@RequestParam(name = "recipients", required = true) String recipients, @RequestParam(name = "subject", required = true) String subject, @RequestParam(name = "content", required = true) String content, @RequestParam(name = "index", required = true) String index, Principal principal, Model model) {
         int idx = Integer.parseInt(index);
-        UserHandle user = repository.findUserHandleByUsername(principal.getName());
-        List<BaseAction> actions= user.getActions();
+        UserHandle user = userHandleManager.loadUserByUserName(principal.getName());
+        List<BaseAction> actions = user.getActions();
         List<String> recipient_list = new ArrayList<>();
         String[] reci_array = recipients.split(" ");
         for (String r :
@@ -88,17 +92,18 @@ public class EmailController {
         EmailAction action = new EmailAction(recipient_list, subject, false, content);
         actions.set(idx, action);
         user.setActions(actions);
-        repository.save(user);
+        userHandleManager.updateUser(user);
         return new RedirectView("getEmails");
     }
+
     @GetMapping("/deleteEmail")
-    public RedirectView deleteEmail(@RequestParam(name="idx", required=true) String index, Principal principal) {
+    public RedirectView deleteEmail(@RequestParam(name = "idx", required = true) String index, Principal principal) {
         int idx = Integer.parseInt(index);
-        UserHandle user = repository.findUserHandleByUsername(principal.getName());
-        List<BaseAction> actions= user.getActions();
+        UserHandle user = userHandleManager.loadUserByUserName(principal.getName());
+        List<BaseAction> actions = user.getActions();
         actions.remove(idx);
         user.setActions(actions);
-        repository.save(user);
+        userHandleManager.updateUser(user);
         return new RedirectView("getEmails");
     }
 
--- Backend/src/main/java/de/digiwill/controller/SendLifeSignController.java
+++ Backend/src/main/java/de/digiwill/controller/SendLifeSignController.java
@@ -1,7 +1,7 @@
 package de.digiwill.controller;
 
 import de.digiwill.model.UserHandle;
-import de.digiwill.repository.UserHandleRepository;
+import de.digiwill.model.UserHandleManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,15 +13,15 @@ import java.util.Date;
 public class SendLifeSignController {
 
     @Autowired
-    UserHandleRepository repository;
+    UserHandleManager userHandleManager;
 
     @PostMapping("/lifesign")
     void sendLifeSign(Principal principal) {
         String username = principal.getName();
-        UserHandle userHandle = repository.findUserHandleByUsername(username);
+        UserHandle userHandle = userHandleManager.loadUserByUserName(username);
         long currentTime = new Date().getTime();
         userHandle.setLastSignOfLife(currentTime);
-        repository.save(userHandle);
+        userHandleManager.updateUser(userHandle);
     }
 
 }
