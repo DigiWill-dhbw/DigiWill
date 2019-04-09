bfe5b9913544e0e5e30b85ffdcac9af90a3cc83e
Jannik Moell <jannik.moell@sap.com>
2019-04-09 09:08:07 +0200

    [Backend] [Ref] DW-116 Removed unneseccary code

--- src/main/java/de/digiwill/repository/UserHandleRepositoryImpl.java
+++ src/main/java/de/digiwill/repository/UserHandleRepositoryImpl.java
@@ -1,12 +1,6 @@
 package de.digiwill.repository;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
-
 public class UserHandleRepositoryImpl implements UserHandleRepositoryCustom {
-
-    @Autowired
-    private MongoTemplate mongoTemplate;
 
     @Override
     public boolean checkLogin() {
