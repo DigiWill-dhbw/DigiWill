bdc5f732c5d80e4f1620e89206b3762ad5cbbc7a
Jannik Möll <jannik.moell@sap.com>
2019-04-26 10:40:26 +0200

    Removed unused imports

--- src/test/java/de/digiwill/steps/LoginFeatureTest.java
+++ src/test/java/de/digiwill/steps/LoginFeatureTest.java
@@ -4,7 +4,6 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import de.digiwill.SpringBootBaseIntegrationTest;
-import de.digiwill.model.EmailAction;
 import de.digiwill.model.UserHandle;
 import de.digiwill.service.UserHandleManager;
 import org.openqa.selenium.By;
@@ -12,7 +11,6 @@ import org.openqa.selenium.WebDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.*;
