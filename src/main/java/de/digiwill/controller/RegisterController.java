34104f9088b7f115af609762be9b1cf9fd590d4c
Christian Schweigel <christian.david.schweigel@sap.com>
2019-04-30 12:37:02 +0200

    [Backend][Misc] Redirects to Home page from register if logged in
    Took 19 minutes

--- src/main/java/de/digiwill/controller/RegisterController.java
+++ src/main/java/de/digiwill/controller/RegisterController.java
@@ -2,6 +2,8 @@ package de.digiwill.controller;
 
 import de.digiwill.service.registration.RegistrationResponse;
 import de.digiwill.service.registration.RegistrationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -13,12 +15,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.security.Principal;
+
 
 @Controller
 public class RegisterController {
 
     @Autowired
     private RegistrationService registrationService;
+    private Logger logger = LoggerFactory.getLogger(GreetingController.class);
 
     @RequestMapping(value = "/register", method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
