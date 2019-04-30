da5b54fb6474164064b4a89b43777f32a88bae89
Christian Schweigel <christian.david.schweigel@sap.com>
2019-04-30 12:37:31 +0200

    [Backend][Misc] Removed unused logger
    Took 31 seconds

--- src/main/java/de/digiwill/controller/RegisterController.java
+++ src/main/java/de/digiwill/controller/RegisterController.java
@@ -23,7 +23,6 @@ public class RegisterController {
 
     @Autowired
     private RegistrationService registrationService;
-    private Logger logger = LoggerFactory.getLogger(GreetingController.class);
 
     @RequestMapping(value = "/register", method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
