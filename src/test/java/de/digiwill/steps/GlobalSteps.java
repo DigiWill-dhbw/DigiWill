c8d492d63f0f3f7a74070f5f243f2b06e697ee88
Kucki99 <kuck.robin@gmail.com>
2019-05-05 18:20:58 +0200

    DW-180 [Backend][Misc] added geckodriver path + modified Jenkinsfile for parallel Selenium tests

--- src/test/java/de/digiwill/steps/GlobalSteps.java
+++ src/test/java/de/digiwill/steps/GlobalSteps.java
@@ -4,24 +4,44 @@ import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import de.digiwill.SpringBootBaseIntegrationTest;
 import de.digiwill.util.SeleniumDriverUtils;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 
-public class GlobalSteps {
+import java.util.concurrent.TimeUnit;
 
+public class GlobalSteps {
 
     @Autowired
     private SpringBootBaseIntegrationTest springBootBaseIntegrationTest;
 
     @Before("@uitest")
     public void beforeUITest() {
-        System.setProperty("webdriver.chrome.driver", SeleniumDriverUtils.getChromeDriverPath());
-        springBootBaseIntegrationTest.setWebDriver(new ChromeDriver(SeleniumDriverUtils.getChromeOptions()));
+        switch (System.getProperty("browser", "chrome")) {
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", SeleniumDriverUtils.getChromeDriverPath());
+                springBootBaseIntegrationTest.setWebDriver(new ChromeDriver(SeleniumDriverUtils.getChromeOptions()));
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver", SeleniumDriverUtils.getFirefoxDriverPath());
+                springBootBaseIntegrationTest.setWebDriver(new FirefoxDriver(SeleniumDriverUtils.getFirefoxOptions()));
+                break;
+            case "edge":
+                //TODO add edge
+                break;
+        }
     }
 
     @After("@uitest")
     public void afterUITest() {
         springBootBaseIntegrationTest.getWebDriver().quit();
         springBootBaseIntegrationTest.dropUsers();
+    }
+
+    private void settingImplicitWait(WebDriver driver) {
+        //setting implicit wait for driver
+        driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
     }
 }
