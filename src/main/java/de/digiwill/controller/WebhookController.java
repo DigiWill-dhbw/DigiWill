99b0f23b47c58a83610d680dca6f409776f30e09
Christian Schweigel <christian.david.schweigel@sap.com>
2019-05-12 16:11:10 +0200

    [Backend][Feat] Added webhook add user interface
    Took 38 minutes

--- src/main/java/de/digiwill/controller/WebhookController.java
+++ src/main/java/de/digiwill/controller/WebhookController.java
@@ -4,6 +4,7 @@ package de.digiwill.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,11 +13,12 @@ import java.security.Principal;
 
 @Controller
 public class WebhookController {
-    @RequestMapping(value="/webhook", method = RequestMethod.POST)
+    @RequestMapping(value="/saveWebhook", method = RequestMethod.POST)
     public String updateWebhook(@RequestBody MultiValueMap<String, String> formData, Model model) {
         return "index";
     }
 
+    @GetMapping(value="/webhook")
     public String webhook(Model model, Principal principal) {
         return "webhook";
     }
