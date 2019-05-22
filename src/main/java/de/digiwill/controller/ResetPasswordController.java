481a480ad4fc8eba850b72fbf2b1cdcd31821f04
Jannik Möll <42173084+MoellJ@users.noreply.github.com>
2019-05-22 13:58:12 +0200

    [Frontend][Feat] DW-201 Added feature to request password reset (#90)
    * [Frontend][Feat] DW-201 Added feature to request password reset
    
    * Removed unused import
    
    * [Frontend][Feat] DW-201 Fixed minor issues

---
+++ src/main/java/de/digiwill/controller/ResetPasswordController.java
@@ -0,0 +1,49 @@
+package de.digiwill.controller;
+
+import de.digiwill.exception.EmailException;
+import de.digiwill.model.EmailResetHandle;
+import de.digiwill.model.UserHandle;
+import de.digiwill.repository.EmailResponseHandleRepository;
+import de.digiwill.service.EmailDispatcher;
+import de.digiwill.service.EmailResponseHandleManager;
+import de.digiwill.service.UserHandleManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class ResetPasswordController {
+
+    @Autowired
+    private UserHandleManager userHandleManager;
+    @Autowired
+    private EmailResponseHandleManager emailResponseHandleManager;
+    @Autowired
+    private EmailDispatcher emailDispatcher;
+    @Autowired
+    private EmailResponseHandleRepository emailResponseHandleRepository;
+
+
+    @RequestMapping(value = "/requestPasswordReset", method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String requestPasswordReset(@RequestBody MultiValueMap<String, String> formData, Model model, RedirectAttributes redirectAttrs) {
+
+        EmailResetHandle emailResetHandle = null;
+        try {
+            UserHandle userHandle = userHandleManager.loadUserByEmailAddress(formData.getFirst("email"));
+            emailResetHandle = new EmailResetHandle(userHandle);
+            emailResponseHandleRepository.insert(emailResetHandle);
+
+            emailDispatcher.sendResetEmail(emailResetHandle, userHandle);
+        } catch (EmailException e) {
+            emailResponseHandleManager.deleteEmailResponseHandle(emailResetHandle);
+        } catch (Exception e) {
+        }
+        return "redirect:/";
+    }
+
+}
