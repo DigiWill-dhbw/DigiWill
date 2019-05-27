47a776aeeb59dbf0462918450a3c46977a756fe1
Jannik Möll <jannik.moell@sap.com>
2019-05-27 11:18:01 +0200

    Changed scope of autowired WebhookService

--- src/main/java/de/digiwill/model/WebhookAction.java
+++ src/main/java/de/digiwill/model/WebhookAction.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class WebhookAction extends BaseAction {
 
     @Autowired
-    WebhookService webhookService;
+    private WebhookService webhookService;
 
     private String url;
 
