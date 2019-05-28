23da5f4fcd12678a1d190fe3403ccade40c2a883
Christian Schweigel <christian.david.schweigel@sap.com>
2019-05-28 10:44:43 +0200

    [Backend][Misc] Removed unused import
    Took 1 minute

--- src/main/java/de/digiwill/controller/EmailController.java
+++ src/main/java/de/digiwill/controller/EmailController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.security.Principal;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
