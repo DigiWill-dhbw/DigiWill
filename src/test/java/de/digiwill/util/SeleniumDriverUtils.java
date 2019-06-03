96ea0971514f5f7effbdecf6cf3e0c9f45cf42e6
Kucki99 <kuck.robin@gmail.com>
2019-06-03 09:58:25 +0200

    Revert "DW-171 [Backend][Test] added page load timeout"
    This reverts commit 98c33ad52d3b865d45d2ba1e6c090c5f9df23934.

--- src/test/java/de/digiwill/util/SeleniumDriverUtils.java
+++ src/test/java/de/digiwill/util/SeleniumDriverUtils.java
@@ -39,6 +39,7 @@ public class SeleniumDriverUtils {
         if (SystemUtils.IS_OS_LINUX) {
             options.addArguments("--headless");
         }
+        options.addArguments("--headless");
         return options;
     }
 
