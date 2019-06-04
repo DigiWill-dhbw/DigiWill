b1f79c587b8cf23ef41a8b0065fd2df14e766eec
Christian Schweigel <christian.david.schweigel@sap.com>
2019-06-04 10:36:06 +0200

    [Frontend][Misc] Adding global comment
    Took 4 minutes

--- src/main/resources/static/js/webhook_check.js
+++ src/main/resources/static/js/webhook_check.js
@@ -1,3 +1,5 @@
+/* global postAjax */
+
 function onWebhookDelete() {
     var csrfToken = document.getElementsByName("_csrf")[0].value;
     postAjax("/deleteWebhook", "_csrf=" + csrfToken, function (data) {
