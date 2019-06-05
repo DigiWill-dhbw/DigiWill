a206fc7769d9b1485563283d53f3df37fc386d49
Jannik Möll <jannik.moell@sap.com>
2019-06-05 10:41:09 +0200

    [Frontend][Test] Removed unused import

--- src/test/java/de/digiwill/steps/EmailCRUDSteps.java
+++ src/test/java/de/digiwill/steps/EmailCRUDSteps.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
