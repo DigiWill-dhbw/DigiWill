1291ddf6427c866d4d63cde1711477d610295550
Christian Schweigel <christian.david.schweigel@sap.com>
2019-06-17 10:42:34 +0200

    [Backend] [Feat] Add LifeSign feedback
    Took 26 minutes

--- src/main/java/de/digiwill/controller/SendLifeSignController.java
+++ src/main/java/de/digiwill/controller/SendLifeSignController.java
@@ -4,7 +4,9 @@ import de.digiwill.model.UserHandle;
 import de.digiwill.service.UserHandleManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.security.Principal;
 
@@ -15,11 +17,14 @@ public class SendLifeSignController {
     private UserHandleManager userHandleManager;
 
     @PostMapping("/lifeSign")
-    public String sendLifeSign(Principal principal) {
+    public String sendLifeSign(Principal principal, Model model) {
         String emailAddress = principal.getName();
         UserHandle userHandle = userHandleManager.loadUserByEmailAddress(emailAddress);
         userHandle.sendSignOfLife();
         userHandleManager.updateUser(userHandle);
+        model.addAttribute("hasToast", true);
+        model.addAttribute("errorText", "We registered your sign of life");
+        model.addAttribute("errorLbl", "✔");
         return "index";
     }
 
