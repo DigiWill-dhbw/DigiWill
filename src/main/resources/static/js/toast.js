b367d2a2323838881b11a3daddc37e71648d763a
Christian Schweigel <33018263+swip3798@users.noreply.github.com>
2019-06-17 12:20:53 +0200

    DW-130 (#128)
    * [Backend] [Feat] Add LifeSign feedback
    
    Took 26 minutes
    
    * [Frontend] [Feat] Click to logo directs to index
    
    Took 43 seconds
    
    * [Backend][Misc] Removed unused import
    
    Took 3 minutes
    
    * [Backend][Misc] Fix footer in profile view
    
    Took 6 minutes

--- src/main/resources/static/js/toast.js
+++ src/main/resources/static/js/toast.js
@@ -1,8 +1,11 @@
-function launchToast(errorText) {
+function launchToast(errorText, errorLbl) {
     // errorText is optional
     var x = document.getElementById("toast");
     if (errorText) {
         document.getElementById("desc").appendChild(document.createTextNode(errorText));
+    }
+    if (errorLbl) {
+        document.getElementById("img").innerText = errorLbl;
     }
     x.className = "show";
 }
